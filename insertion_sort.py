216587108895937fae3a091b0ed2c7d9aeb6d916
Indraneel <neilpatilcoldplay@gmail.com>
2021-09-02 18:51:04 +0530

    Reverse insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,7 +6,7 @@ class InsertionSort:
     def __init__(self,input):
         self.data = input
         self.print_data()
-        self.run_sort_algorithm()
+        self.run_reverse_sort_algorithm()
         self.print_data()
 
     def print_data(self):
@@ -19,6 +19,19 @@ class InsertionSort:
             j=i-1
         
             while(self.data[j]>key and j>=0):
+                self.data[j+1]=self.data[j]
+                j=j-1
+
+            j=j+1
+            self.data[j]=key
+    
+    def run_reverse_sort_algorithm(self):
+        
+        for i in range(1,len(self.data)):
+            key = self.data[i]
+            j=i-1
+        
+            while(self.data[j]<key and j>=0):
                 self.data[j+1]=self.data[j]
                 j=j-1
 
